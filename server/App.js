4230e63c1515fbc1721fdf75789f943ce352c64d
ayaelgamall <aya.elgamal14@gmail.com>
2021-11-05 18:34:58 +0200

    core correct

--- server/App.js
+++ server/App.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 var cors= require('cors');
 
-app.use(cors);
+app.use(cors());
 
 const usersRouter = require('./routes/adminController.js');
 app.use('/Admin', usersRouter);
