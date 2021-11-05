f54f79416f35fc9e6440e93a9a3dbe12ce9f232b
khadijabadrawy <khadija.badrawy@incorta.com>
2021-11-05 12:56:06 +0200

    initial step front end

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,11 +1,23 @@
-//import React, { Component } from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router , Route} from 'react-router-dom';
+import axios from 'axios';
+import {useState,useEffect} from 'react';
+import Button from '@material-ui/core/Button';
 
 
 function App() {
-  return(
-    <div> "hey there" </div>
-  )
-  
-}
+ 
+  const[list, setList]= useState([]); 
 
-export default App;
+ useEffect(()=>{
+    
+  axios.get('http://localhost:8000/Admin/allFlights')
+  .then(res=> {setList(res.data);console.log(res)}).catch(err=>console.log(err))
+ },[]);
+
+   return (
+     list.map(a=>{return (<div><label>{a.flightNo}</label><br/></div>)})
+     );
+   };
+ export default App;
