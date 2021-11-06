2fb745b0bb0fcb6919eefe095561c71cf1a80a2d
khadijabadrawy <khadija.badrawy@incorta.com>
2021-11-06 22:37:52 +0200

    almost done view all flights front end

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import {BrowserRouter as Router , Route} from 'react-router-dom';
-import axios from 'axios';
-import {useState,useEffect} from 'react';
-import allFlights from './components/viewAllFlights.js';
-
+import  ViewAllFlights from './components/viewAllFlights.js';
 
 export default function CustomizedTables() {
- 
+
   return (
-     <Router>
-      <Route path ='/Admin/allFlights' component={allFlights}></Route>
+    <Router>
+      <Route path ={"/AllFlights"} component={ViewAllFlights}></Route>
      </Router>
   );
 }
+
 
