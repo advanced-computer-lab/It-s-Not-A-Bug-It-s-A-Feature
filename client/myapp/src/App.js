5af00c3dab824b3a35cbf2632bbf5945407d2808
khadijabadrawy <khadija.badrawy@incorta.com>
2021-11-07 13:39:10 +0200

    path of search and dependencies added

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router , Route} from 'react-router-dom';
 import  ViewAllFlights from './components/viewAllFlights.js';
-
+import Search from './components/searchComponent.js'
 export default function CustomizedTables() {
 
   return (
     <Router>
       <Route path ={"/AllFlights"} component={ViewAllFlights}></Route>
+      <Route path ={"/Home"} component={Search}></Route>
      </Router>
   );
 }
