22446df218298d732141a3efd1cdc42aff609ade
BASANT <basant_allam@hotmail.com>
2021-11-07 03:20:19 +0200

    component function for consistency

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router , Route} from 'react-router-dom';
 import  ViewAllFlights from './components/viewAllFlights.js';
-
+import SearchFlights from './components/SearchFlights.js';
 export default function CustomizedTables() {
 
   return (
     <Router>
-      <Route path ={"/AllFlights"} component={ViewAllFlights}></Route>
-      <Route path='/ViewFlights'component={ViewFlights}/>
+      <Route path ={"/AllFlights"} component={ViewAllFlights}/>
+      <Route path={"/ViewFlights"} component={SearchFlights}/>
 
      </Router>
   );
