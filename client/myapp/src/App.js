1801d013a1ff35b268df59d6089d05db6acb6c14
BASANT <basant_allam@hotmail.com>
2021-11-07 03:04:19 +0200

    Update App.js

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -7,6 +7,8 @@ export default function CustomizedTables() {
   return (
     <Router>
       <Route path ={"/AllFlights"} component={ViewAllFlights}></Route>
+      <Route path='/ViewFlights'component={ViewFlights}/>
+
      </Router>
   );
 }
