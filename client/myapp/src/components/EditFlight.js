4d73d614ff5ca9d655e0d062b24ef89e7a5909b5
Iman <iman.osama2000@gmail.com>
2021-11-07 18:28:57 +0200

    refactored code

--- client/myapp/src/components/EditFlight.js
+++ client/myapp/src/components/EditFlight.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom';
-import { useParams } from "react-router";
 import Box from '@mui/material/Box';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
@@ -11,8 +8,6 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import CircularProgress from '@mui/material/CircularProgress';
 import Paper from '@material-ui/core/Paper';
-import { styled } from '@material-ui/core/styles';
-// import DeleteIcon from '@material-ui/core/Button'
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
@@ -53,19 +48,15 @@ function Copyright() {
   );
 }
 
-
-
-
-
 const theme = createTheme();
 
-function Checkout() {
+// function Checkout() {
   
 
-  const handleNext = () => {
+//   const handleNext = () => {
     
-  };
-}
+//   };
+// }
 export default class EditFlight extends Component {
 
     constructor(props) {
@@ -83,22 +74,7 @@ export default class EditFlight extends Component {
         
         this.onSubmit = this.onSubmit.bind(this);
     
-        // var self = this;
-        // var f = axios.get('http://localhost:8000/admin/editFlight/' + this.props.match.params.id)
-        //   .then(res => console.log(res.data), this.setState({events: res.data}));
-        // this.state = {
-          // flightNo: 0,
-          // departureDate : new Date(2018, 11, 24, 10, 33, 30, 0),
-          // arrivalDate : new Date(2018, 11, 25, 10, 33, 30, 0),
-          // economySeats : 4,
-          // businessSeats : 2,
-          // arrivalAirport : 'e',
-          // departureAirport : 'e',
-          // departureTerminal : 'e',
-          // arrivalTerminal : 'e'
-          
-        // }
-          // var self = this;
+        
           this.state = {
             flightNo: 0,
             departureDate : new Date(2000, 1, 1, 1, 33, 30, 0),
@@ -134,22 +110,11 @@ export default class EditFlight extends Component {
           });
 
 
-      //     axios.get('http://localhost:8000/admin/editFlight/' + this.props.match.params.id)
-      // .then(res => {
-      //   const persons = res.data;
-      //   this.setState({ persons });
-      // })
-
-          
-        
 
 
         
       }
 
-      // componentDidUpdate(props, state){
-        
-      // }
       onChangeFlightNo(e) {
         this.setState({
           flightNo: e.target.value
@@ -257,6 +222,7 @@ export default class EditFlight extends Component {
       </AppBar>
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+          {/* <Paper> */}
           <Typography component="h1" variant="h4" align="center">
             Edit Flight #{this.state.flightNo}
           </Typography>
@@ -264,139 +230,13 @@ export default class EditFlight extends Component {
           
               <React.Fragment>
               {DataForm(this)}
-              {/* {<React.Fragment>
-      
-      <Grid container spacing={4}>
-        <Grid item xs={12} >
-        <TextField
-          required
-          id="FlightNo"
-          fullWidth
-          variant="standard"
-          label="Flight Number"
-          type="number"
-          defaultValue= {this.state.flightNo}
-          onChange={this.onChangeFlightNo}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-          required
-          id="filled-required"
-          fullWidth
-          variant="standard"
-          label="Number of Economy Seats"
-          type="number"
-          defaultValue= {this.state.economySeats}
-          onChange={this.onChangeEconSeats}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-          required
-          id="filled-required"
-          fullWidth
-          variant="standard"
-          label="Number of Business Seats"
-          type="number"
-          defaultValue= {this.state.businessSeats}
-          onChange={this.onChangeBusinessSeats}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-          required
-          id="filled-required"
-          fullWidth
-          variant="standard"
-          label="dept ariport"
-          defaultValue= {this.state.departureAirport}
-          onChange={this.onChangeDeptAirport}
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-          required
-          id="filled-required"
-          fullWidth
-          variant="standard"
-          label="arrival ariport"
-          defaultValue= {this.state.arrivalAirport}
-          onChange={this.onChangeArrAirport}
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-          required
-          id="filled-required"
-          fullWidth
-          variant="standard"
-          label="Departure Terminal"
-          defaultValue= {this.state.departureTerminal}
-          onChange={this.onChangeDeptTerminal}
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-          required
-          id="filled-required"
-          fullWidth
-          variant="standard"
-          label="Arrival Terminal"
-          defaultValue= {this.state.arrivalTerminal}
-          onChange={this.onChangeArrTerminal}
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DateTimePicker
-            renderInput={(props) => <TextField {...props} 
-            required 
-            id="departureDate"
-            label="Departure date & time"
-            fullWidth
-            variant="standard" />}
-            value={this.state.departureDate}
-            onChange={(newValue) => {
-              this.onChangeDeptDate(newValue);
-            }}            />
-          </LocalizationProvider>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DateTimePicker
-            renderInput={(props) => <TextField {...props} 
-            required 
-            id="arrivalDate"
-            label="Arrival date & time"
-            fullWidth
-            variant="standard" />}
-            value={this.state.arrivalDate}
-            onChange={(newValue) => {
-              this.onChangeArrDate(newValue);
-            }}          />
-          </LocalizationProvider>
-        </Grid>
-       
-      
-      </Grid>
-    </React.Fragment>  } */}
 
-    <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
                 
-                  <Button variant="contained" type="submit" onClick={this.onSubmit} sx={{ mt: 3, ml: 1 }}> Save </Button>
+            <Button variant="contained" type="submit" onClick={this.onSubmit} sx={{ mt: 3, ml: 1 }}> Save </Button>
       
                 </Box>
-              </React.Fragment>
+          </React.Fragment>
             
           </React.Fragment>
         </Paper>
@@ -405,144 +245,7 @@ export default class EditFlight extends Component {
     </ThemeProvider>
     )
 
-//////////////////////////////////////////////////////////////////////
-    // if (this.state.flightNo === 0)
-    // return(
-    //   <Box sx={{ display: 'flex' }}>
-    //   <CircularProgress />
-    // </Box>
-    // )
-    // else
     
-    //       return (
-
-    // <div>
-            
-    //   <form onSubmit={this.onSubmit}>
-    //     <Box
-    //   component="form"
-    //   sx={{
-    //     '& .MuiTextField-root': { m: 1, width: '25ch' },
-    //   }}
-    //   noValidate
-    //   autoComplete="off"
-    // >
-    //   <div>
-    //   <TextField
-    //       required
-    //       id="FlightNo"
-    //       fullWidth
-    //       variant="standard"
-    //       label="Flight Number"
-    //       type="number"
-    //       defaultValue= {this.state.flightNo}
-    //       onChange={this.onChangeFlightNo}
-    //       InputLabelProps={{
-    //         shrink: true,
-    //       }}
-    //     />
-    //     <TextField
-    //       required
-    //       id="filled-required"
-    //       fullWidth
-    //       variant="standard"
-    //       label="Number of Economy Seats"
-    //       type="number"
-    //       defaultValue= {this.state.economySeats}
-    //       onChange={this.onChangeEconSeats}
-    //       InputLabelProps={{
-    //         shrink: true,
-    //       }}
-    //     />
-    //     <TextField
-    //       required
-    //       id="filled-required"
-    //       fullWidth
-    //       variant="standard"
-    //       label="Number of Business Seats"
-    //       type="number"
-    //       defaultValue= {this.state.businessSeats}
-    //       onChange={this.onChangeBusinessSeats}
-    //       InputLabelProps={{
-    //         shrink: true,
-    //       }}
-    //     />
-    //     <TextField
-    //       required
-    //       id="filled-required"
-    //       fullWidth
-    //       variant="standard"
-    //       label="dept ariport"
-    //       defaultValue= {this.state.departureAirport}
-    //       onChange={this.onChangeDeptAirport}
-    //     />
-    //     <TextField
-    //       required
-    //       id="filled-required"
-    //       fullWidth
-    //       variant="standard"
-    //       label="arrival ariport"
-    //       defaultValue= {this.state.arrivalAirport}
-    //       onChange={this.onChangeArrAirport}
-    //     />
-    //     <TextField
-    //       required
-    //       id="filled-required"
-    //       fullWidth
-    //       variant="standard"
-    //       label="Departure Terminal"
-    //       defaultValue= {this.state.departureTerminal}
-    //       onChange={this.onChangeDeptTerminal}
-    //     />
-    //     <TextField
-    //       required
-    //       id="filled-required"
-    //       fullWidth
-    //       variant="standard"
-    //       label="Arrival Terminal"
-    //       defaultValue= {this.state.arrivalTerminal}
-    //       onChange={this.onChangeArrTerminal}
-    //     />
-        
-    //     </div>
-    //     <div>
-    //     <LocalizationProvider dateAdapter={AdapterDateFns}>
-    //     <DateTimePicker
-    //         renderInput={(props) => <TextField {...props} 
-    //         required 
-    //         id="departureDate"
-    //         label="Departure date & time"
-    //         fullWidth
-    //         variant="standard" />}
-    //         value={this.state.departureDate}
-    //         onChange={(newValue) => {
-    //           this.onChangeDeptDate(newValue);
-    //         }}            />
-    //     </LocalizationProvider>
-    //     </div>
-    //     <div>
-    //     <LocalizationProvider dateAdapter={AdapterDateFns}>
-    //     <DateTimePicker
-    //         renderInput={(props) => <TextField {...props} 
-    //         required 
-    //         id="arrivalDate"
-    //         label="Arrival date & time"
-    //         fullWidth
-    //         variant="standard" />}
-    //         value={this.state.arrivalDate}
-    //         onChange={(newValue) => {
-    //           this.onChangeArrDate(newValue);
-    //         }}          />
-    //       </LocalizationProvider>
-    //     </div>
-    //     </Box>
-        
-    //     <Button variant="contained" type="submit" onClick={this.onSubmit} sx={{ mt: 3, ml: 1 }}> Save </Button>
-        
-    //   </form>
-      
-    // </div>
-    //     )
     }
     
 }
@@ -555,10 +258,11 @@ function DataForm(self){
         <TextField
           required
           id="FlightNo"
-          fullWidth
-          variant="standard"
+          name="flightNo"
           label="Flight Number"
           type="number"
+          fullWidth
+          variant="standard"          
           defaultValue= {self.state.flightNo}
           onChange={self.onChangeFlightNo}
           
