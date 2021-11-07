18646f455e2e9617ca1783d145e9691a003f29a5
BASANT <basant_allam@hotmail.com>
2021-11-07 16:02:00 +0200

    Update searchComponent.js

--- client/myapp/src/components/searchComponent.js
+++ client/myapp/src/components/searchComponent.js
@@ -20,6 +20,33 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+function onChangeFlightNo (){
+
+}
+function onChangeArrivalAirport (){
+  
+}
+function onChangeDepartureAirport(){
+  
+}
+function onChangeArrivalTerminal (){
+  
+}
+function onChangeDepartureTerminal (){
+  
+}
+function onChangeArrivalTime (){
+  
+}
+function onChangeDepartureTime (){
+  
+}
+function onChangeArrivalDate (){
+  
+}
+function onChangeDepartureDate (){
+  
+}
 export default function InputWithIcon() {
     const [value, setValue] = React.useState([null, null]);
   return (
@@ -29,16 +56,25 @@ export default function InputWithIcon() {
     
         <Box  sx={{ display: 'flex', alignItems: 'flex-end' }}>
        <Location sx={{ color: 'action.active', mr: 1, my: 2.8 }} />
-        <TextField id="input-with-sx" label="City or Airport" variant="standard" 
-        helperText="Leaving from"/>
+       <TextField 
+        id="input-with-sx" 
+        label="City or Airport" 
+        variant="standard" 
+        helperText="Leaving from"
+        onChange={this.onChangeDepartureAirport}
+        />
         </Box>
   
     <div><SwapHorizIcon sx={{color: 'action.active' , mt:2.5}} /></div>
     
     <Box  sx={{ display: 'flex', alignItems: 'flex-end' }}>
      <Location sx={{ color: 'action.active', mr: 1, my: 2.8 }} />
-        <TextField id="input-with-sx" label="City or Airport" variant="standard" 
+     <TextField id="input-with-sx" 
+        label="City or Airport" 
+        variant="standard" 
         helperText="Going to" 
+        onChange={this.onChangeArrivalAirport}
+
         />
       </Box> 
         
@@ -55,13 +91,21 @@ export default function InputWithIcon() {
         renderInput={(startProps, endProps) => (
           <React.Fragment>
            <DateRangeTwoToneIcon sx={{ color: 'action.active', mr: 1, mt: 1.5 }}/>
-             <TextField {...startProps} id="input-with-sx" variant="standard" 
-              helperText="Departing"
+           <TextField {...startProps} 
+             id="input-with-sx" 
+             variant="standard" 
+             helperText="Departing"
+             onChange={this.onChangeDepartureDate}
             />
             <Box sx={{ mx: 3, my :0.5 }}>to</Box>
             <DateRangeTwoToneIcon sx={{ color: 'action.active', mr: 1, mt: 1.5 }}/>
-            <TextField {...endProps} id="input-with-sx"  variant="standard" 
-            helperText="Arrival" placeholder='mm/dd/yy'
+            <TextField {...endProps} 
+            id="input-with-sx"  
+            variant="standard" 
+            helperText="Arrival" 
+            placeholder='mm/dd/yy'
+            onChange={this.onChangeArrivalDate}
+
             />
           </React.Fragment>
         )}
@@ -77,6 +121,7 @@ export default function InputWithIcon() {
         id="time1"
         type="time"
         variant="standard" 
+        onChange={this.onChangeDepartureTime}
         InputLabelProps={{
           shrink: true,
         }}
@@ -92,6 +137,7 @@ export default function InputWithIcon() {
                 id="time2"
                 type="time"
                 variant="standard" 
+                onChange={this.onChangeArrivalTime}
                 InputLabelProps={{
                 shrink: true,
                 }}
@@ -103,19 +149,33 @@ export default function InputWithIcon() {
             </Box>
 
      <Box  sx={{ display: 'flex', alignItems: 'right',mr:1.5 }}>
-        <TextField id="input-with-sx" label="Terminal No" variant="standard" 
+
+     <TextField 
+        id="input-with-sx" 
+        label="Terminal No" 
+        variant="standard" 
         helperText="Departing"/>
+        onChange={this.onChangeDepartureTerminal}
+
         </Box>
   
     
       <Box  sx={{ display: 'flex', alignItems: 'right' ,mr:1.5}}>
-        <TextField id="input-with-sx" label="Terminal No" variant="standard" 
+      <TextField 
+        id="input-with-sx" 
+        label="Terminal No" 
+        variant="standard" 
         helperText="Arrial" 
+        onChange={this.onChangeArrivalTerminal}
         />
       </Box> 
       <Box  sx={{ display: 'flex', alignItems: 'right',mr:1.5}}>
-        <TextField id="input-with-sx" label="Flight No" variant="standard" 
-       
+      <TextField 
+        id="input-with-sx" 
+        label="Flight No" 
+        variant="standard" 
+        onChange={this.onChangeFlightNo}
+
         />
       </Box> 
     </Stack>
@@ -128,4 +188,3 @@ export default function InputWithIcon() {
 
   );
 }
-
