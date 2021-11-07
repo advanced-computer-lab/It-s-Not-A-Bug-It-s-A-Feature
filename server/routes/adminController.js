84983fbd2245f933583e0ac5d3999f4ff7da8e2f
BASANT <basant_allam@hotmail.com>
2021-11-07 12:35:27 +0200

    Update adminController.js

--- server/routes/adminController.js
+++ server/routes/adminController.js
@@ -94,4 +94,15 @@ router.route('/').get((req, res) => {
        Flights.find(anded, 'flightNo departureDate arrivalDate economySeats businessSeats arrivalAirport departureAirport departureTerminal arrivalTerminal').then( data => res.send(data));
 
 }); 
+
+router.route('/deleteFlight/:id').delete((req,res)=>{
+  var id = req.params.id;
+  console.log(`Deleting flight ID ${id}`);
+  Flights.findByIdAndRemove(id, req.body)
+        .then((result)=>{
+          res.send("Done!");
+        })
+        .catch(err => res.status(404).json({ error: 'No such flight' }));
+}); 
+
   module.exports = router;
