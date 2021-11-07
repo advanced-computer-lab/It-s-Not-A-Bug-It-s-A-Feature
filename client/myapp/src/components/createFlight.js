72a69cd1a546776c182823c7203a36db495e2e4a
Aya <aya.elgamal14@gmail.com>
2021-11-07 20:13:32 +0200

    createFlight works basic

--- client/myapp/src/components/createFlight.js
+++ client/myapp/src/components/createFlight.js
@@ -15,20 +15,27 @@ import DateTimePicker from '@mui/lab/DateTimePicker';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import {useState, useEffect} from 'react';
+import axios from 'axios';
+
 
 
 export default function CreateFlight() {
   const [flightData, setFlight] =useState({
       flightNo:"",
-      eSeatsNo:"",bSeatsNo:"",departureAirport:"",arrivalAirport:"",departureTerminal:"",arrivalTerminal:"",
+      economySeats:"",businessSeats:"",departureAirport:"",arrivalAirport:"",departureTerminal:"",arrivalTerminal:"",
       departureDate:"",arrivalDate:""
     });
    
+   
     
-     const handleNext = () => { 
-       console.log(flightData);
-       console.log("Handle Next");
-      
+     const onSubmit = (e) => { 
+      e.preventDefault();
+  
+  
+      axios.post('http://localhost:8000/admin/createFlight/' , flightData)
+        .then(res => console.log(res.data));
+  
+      window.location = '/';
       };
   
     return (
@@ -48,8 +55,7 @@ export default function CreateFlight() {
   
                     <Button
                       variant="contained"
-                      onClick={(e) => {handleNext(e);
-                      console.log("onClick");
+                      onClick={(e) => {onSubmit(e);
                       }}
                       sx={{ mt: 3, ml: 1 }}
                     >
@@ -107,72 +113,13 @@ function myAppBar(){
 
 const theme = createTheme();
 
-// function Layout() {
-//   const [flightData, setFlight] =useState({
-//     flightNo:null,
-//     eSeatsNo:null,bSeatsNo:null,departureAirport:null,arrivalAirport:null,departureTerminal:null,arrivalTerminal:null,
-//     departureDate:null,arrivalDate:null
-//   });
- 
-  
-//    const handleNext = () => { 
-//     // console.log(flightData);
-//     // console.log(f.flightNo
-//     //   //,eSeatsNo,bSeatsNo,departureAirport,arrivalAirport,departureTerminal,arrivalTerminal,departureDate,arrivalDate
-//     //   );
-//     };
 
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <CssBaseline />
-//       {myAppBar()}
-//       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-//         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-//           <Typography component="h1" variant="h4" align="center">
-//             Create Flight
-//           </Typography>
-//           <React.Fragment>
-          
-//               <React.Fragment>
-//                 {DataForm(flightData,setFlight)}
-//                 <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-
-//                   <Button
-//                     variant="contained"
-//                     onClick={handleNext()}
-//                     sx={{ mt: 3, ml: 1 }}
-//                   >
-//                     Create
-//                   </Button>
-//                 </Box>
-//               </React.Fragment>
-            
-//           </React.Fragment>
-//         </Paper>
-//         <Copyright />
-//       </Container>
-//     </ThemeProvider>
-//   );
-// }
 
 
 
 
 function DataForm( d,setData) {
-  
-  // const [flightNo, setflightNo] = useState(null);
-  // const [eSeatsNo, setESeatsNo] = useState(null);
-  // const [bSeatsNo, setBSeatsNo] = useState(null);
-  // const [departureAirport, setDepartureAirport] = React.useState(null);
-  // const [arrivalAirport, setArrivalAirport] = React.useState(null);
-  // const [departureTerminal, setDepartureTerminal] = React.useState(null);
-  // const [arrivalTerminal, setArrivalTerminal] = React.useState(null);
-  // const [departureDate, setDepartureDate] = React.useState(null);
-  // const [arrivalDate, setArrivalDate] = React.useState(null);
 
-  
-
- 
   
   return (
     <React.Fragment>     
@@ -189,10 +136,7 @@ function DataForm( d,setData) {
             onChange={(event) =>  {
               const {name, value} = event.target;
               setData((prevState => {
-                  console.log({
-                      ...prevState,
-                      [name]: value
-                  });
+                 
                   return {
                       ...prevState,
                       [name]: value
@@ -206,18 +150,14 @@ function DataForm( d,setData) {
           <TextField
             required
             id="economySeats"
-            name="eSeatsNo"
+            name="economySeats"
             label="Number of Economy Seats"
             fullWidth
             variant="standard"
-            value={d.eSeatsNo}
+            value={d.economySeats}
             onChange={(event) =>  {
               const {name, value} = event.target;
               setData((prevState => {
-                  console.log({
-                      ...prevState,
-                      [name]: value
-                  });
                   return {
                       ...prevState,
                       [name]: value
@@ -230,19 +170,15 @@ function DataForm( d,setData) {
         <Grid item xs={12} sm={6}>
           <TextField
             required
-            id="bSeatsNo"
-            name="bSeatsNo"
+            id="businessSeats"
+            name="businessSeats"
             label="Number of Business Seats"
             fullWidth
             variant="standard"
-            value={d.bSeatsNo}
+            value={d.businessSeats}
             onChange={(event) =>  {
               const {name, value} = event.target;
               setData((prevState => {
-                  console.log({
-                      ...prevState,
-                      [name]: value
-                  });
                   return {
                       ...prevState,
                       [name]: value
@@ -264,10 +200,6 @@ function DataForm( d,setData) {
             onChange={(event) =>  {
               const {name, value} = event.target;
               setData((prevState => {
-                  console.log({
-                      ...prevState,
-                      [name]: value
-                  });
                   return {
                       ...prevState,
                       [name]: value
@@ -289,10 +221,6 @@ function DataForm( d,setData) {
             onChange={(event) =>  {
               const {name, value} = event.target;
               setData((prevState => {
-                  console.log({
-                      ...prevState,
-                      [name]: value
-                  });
                   return {
                       ...prevState,
                       [name]: value
@@ -314,10 +242,6 @@ function DataForm( d,setData) {
             onChange={(event) =>  {
               const {name, value} = event.target;
               setData((prevState => {
-                  console.log({
-                      ...prevState,
-                      [name]: value
-                  });
                   return {
                       ...prevState,
                       [name]: value
@@ -339,10 +263,6 @@ function DataForm( d,setData) {
             onChange={(event) =>  {
               const {name, value} = event.target;
               setData((prevState => {
-                  console.log({
-                      ...prevState,
-                      [name]: value
-                  });
                   return {
                       ...prevState,
                       [name]: value
@@ -364,19 +284,12 @@ function DataForm( d,setData) {
             value={d.departureDate}
             onChange={(event) =>  {
               setData((prevState => {
-                  console.log({
-                      ...prevState,
-                      ["departureDate"]: event
-                  });
                   return {
                       ...prevState,
                       ["departureDate"]: event
                   };
               }));
           }}
-            // onChange={(newValue) => {
-            //   setData({[newValue.target.name]:newValue.target.value});
-            // }}
           />
           </LocalizationProvider>
         </Grid>
@@ -396,10 +309,6 @@ function DataForm( d,setData) {
             value={d.arrivalDate}
             onChange={(event) =>  {
               setData((prevState => {
-                  console.log({
-                      ...prevState,
-                      ["arrivalDate"]: event
-                  });
                   return {
                       ...prevState,
                       ["arrivalDate"]: event
