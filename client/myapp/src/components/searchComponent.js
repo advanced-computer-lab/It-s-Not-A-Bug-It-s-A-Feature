3f421a10f5d561e552898f4fc4fbbee029587a06
KHADIJA <khadija.badrawy@gmail.com>
2021-11-09 00:14:10 +0200

    Update searchComponent.js

--- client/myapp/src/components/searchComponent.js
+++ client/myapp/src/components/searchComponent.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-
 import Box from '@material-ui/core/Box';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Link from '@mui/material/Link';
