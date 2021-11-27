268116b49de589dd01e2572707bd80ba926b1123
Aya <aya.elgamal14@gmail.com>
2021-11-28 00:13:48 +0200

    Home Page base

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CreateFlight from './views/admin/createFlight';
 import  ViewAllFlights from './views/admin/viewAllFlights.js';
 
-import  Home from './views/users/home.js';
+import  Home from './views/users/LandingPage/LandingPage.js';
 import  Login from './views/users/login.js';
 // import { render } from "react-dom";
 import EditFlight from './views/admin/EditFlight';
@@ -12,8 +12,9 @@ import Search from './views/admin/searchComponent.js'
 function App() {
   return(
          <Router> 
-           <Route path ={"/"} component={Home}></Route>
+           
            <Route path ={"/login"} component={Login}></Route>
+           <Route path ={"/home"} component={Home}></Route>
            <Route path ={"/admin/allFlights"} component={ViewAllFlights}></Route>
            <Route path='/admin/createFlight' component={CreateFlight}></Route>
            <Route path='/admin/editFlight/:id' component={EditFlight} />
