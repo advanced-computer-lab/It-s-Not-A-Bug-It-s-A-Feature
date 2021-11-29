218319f2f8ba12f887892de06c436e1cbd95d7fe
Aya <aya.elgamal14@gmail.com>
2021-11-29 23:01:19 +0200

    WORKINGGGGGG BASE FRONT VIEW NO SCREENS ONLY BASE!

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -8,14 +8,22 @@ import  Login from './views/users/login.js';
 // import { render } from "react-dom";
 import EditFlight from './views/admin/EditFlight';
 import Search from './views/admin/searchComponent.js'
+//import { ThemeProvider } from "@mui/styles";
 import { ThemeProvider } from '@material-ui/styles';
+
+ import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+//import { createMuiTheme } from '@material-ui/core';
+
+//const baseTheme = createMuiTheme();
+ let theme = createTheme();
+ theme = responsiveFontSizes(theme);
 
 
 
 
 function App() {
   return(
-    
+    <ThemeProvider theme={theme}>
          <Router> 
            
            <Route path ={"/login"} component={Login}></Route>
@@ -25,7 +33,7 @@ function App() {
            <Route path='/admin/editFlight/:id' component={EditFlight} />
            <Route path ={"/admin/search"} component={Search}/>
          </Router> 
-         
+         </ThemeProvider>
 
          );
   }
