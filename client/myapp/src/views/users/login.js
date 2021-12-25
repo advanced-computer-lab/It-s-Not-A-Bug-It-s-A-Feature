383b742c5af050d26610b1787c26bde289990940
Aya <aya.elgamal14@gmail.com>
2021-12-25 22:10:02 +0200

    login push correct

--- client/myapp/src/views/users/login.js
+++ client/myapp/src/views/users/login.js
@@ -50,6 +50,7 @@ export default function Login(props) {
        setmessage(key.message);
        setmessagecolor("info");
     }
+    console.log("state :",key);
     },[]);
 
 
@@ -89,14 +90,15 @@ export default function Login(props) {
                 document.cookie = "jwt=" + tokenWithout;
                 if (res.data.isAdmin === false){
                     console.log("key",key);
-                    if(key!==null){
-                        history.push({
-                            pathname: "/reserveSeats",
-                            state: key
-                
-                        });
+                    if(key==null){
+                        history.push("/profile");
                     }
-                    else history.push("/profile");
+                    else 
+                    history.push({
+                        pathname: "/reserveSeats",
+                        state: key
+            
+                    });
                 }
                 else
                     history.push("/admin/createFlight");
