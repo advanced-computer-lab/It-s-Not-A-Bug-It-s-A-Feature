21de70ce6387e71f1f9e9e13c691dea2a78be0c7
Basant <basant_allam@hotmail.com>
2021-12-25 11:56:06 +0200

    autoformatting back end

--- server/models/Reservation.js
+++ server/models/Reservation.js
@@ -3,48 +3,48 @@ const Flights = require('../models/Flights.js');
 const User = require('../models/User.js');
 
 const UserSchema = new mongoose.Schema({
-  reservationID: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  userID: { 
-    type: String,
-    required: true,
-  },
-  adultsNo: {
-    type: Number,
-    required: true,
-  },
-  childrenNo: {
-    type: Number,
-    required: true,
-  },
-  seatClass: { 
-    type: String,
-    enum : ['Business','Economy'],
-    required: true,
-  },
-  deptFlight: { 
-    type: String,
-    required: true,
-  },
-  arrFlight: { 
-    type: String,
-    required: true,
-  },
-  deptSeats: {
-    type: [Number],
-    required: true,
-  },
-  arrSeats: {
-    type: [Number],
-    required: true,
-  },
-  price: { //calculated
-    type: Number,
-    required: true,
-  }
+    reservationID: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    userID: {
+        type: String,
+        required: true,
+    },
+    adultsNo: {
+        type: Number,
+        required: true,
+    },
+    childrenNo: {
+        type: Number,
+        required: true,
+    },
+    seatClass: {
+        type: String,
+        enum: ['Business', 'Economy'],
+        required: true,
+    },
+    deptFlight: {
+        type: String,
+        required: true,
+    },
+    arrFlight: {
+        type: String,
+        required: true,
+    },
+    deptSeats: {
+        type: [Number],
+        required: true,
+    },
+    arrSeats: {
+        type: [Number],
+        required: true,
+    },
+    price: { //calculated
+        type: Number,
+        required: true,
+    }
 
 });
 
--- server/models/User.js
+++ server/models/User.js
@@ -1,63 +1,63 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  birthDate: {
-    type: Date,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  countryCode: {
-    type: Number,
-    required: true,
-  },
-  phoneNo: {
-    type: Number,
-    required: true,
-  },
-  phoneNoOptional: {
-    type: Number,
-    required: false,
-  },
-  username:{
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password:{ //encrypt
-    type: String,
-    required: true,
-  },
-  nationality:{
-    type: String,
-    required: true,
-  },
-  email:{ //verify?
-    type: String,
-    required: true,
-  },
-  creditCardNo:{ //encrypt
-    type: Number,
-    required: true,
-  },
-  passportNo: {
-    type: String,
-    required: true,
-  },
-  isAdmin:{
-    type: Boolean,
-    required: true,
-  }
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    birthDate: {
+        type: Date,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    countryCode: {
+        type: Number,
+        required: true,
+    },
+    phoneNo: {
+        type: Number,
+        required: true,
+    },
+    phoneNoOptional: {
+        type: Number,
+        required: false,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: { //encrypt
+        type: String,
+        required: true,
+    },
+    nationality: {
+        type: String,
+        required: true,
+    },
+    email: { //verify?
+        type: String,
+        required: true,
+    },
+    creditCardNo: { //encrypt
+        type: Number,
+        required: true,
+    },
+    passportNo: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+    }
 
 });
 
